4733adcb4a4a3593841fb708a04f593be6f40533
verozroke <73778760+verozroke@users.noreply.github.com>
2023-10-18 14:42:09 +0600

    download added

--- pages/index.vue
+++ pages/index.vue
@@ -11,21 +11,25 @@
             label="План эвакуации" variant="outlined"></v-file-input>
           <v-text-field v-model="address" variant="outlined" prepend-icon="mdi-map-marker-outline" :rules="addressRules"
             color="#FA126C" label="Адрес здания и этаж"
-            placeholder="Введите адрес здания. Пример: г. Астана, Қалдаякова 56, 1 этаж"></v-text-field>
-          <div class="flex w-full justify-end">
+            placeholder="Пример: г. Астана, Қалдаякова 56, 1 этаж"></v-text-field>
+          <div class="flex gap-2 w-full justify-end">
             <v-btn type="submit" size="large" class="w-fit px-5" color="#FA126C"
-              style="text-transform: none; letter-spacing: .7px;">Сгенерировать QR
+              style="text-transform: none; letter-spacing: .7px;" prepend-icon="mdi-qrcode-plus">Сгенерировать QR
               код</v-btn>
+            <v-btn v-if="!!currentQR" @click="() => downloadQR()" size="large" class="w-fit px-5" color="#28B0A6"
+              style="text-transform: none; letter-spacing: .7px;" prepend-icon="mdi-download">Скачать</v-btn>
           </div>
         </v-form>
-        <div v-if="qrExist" class="bg-slate-500 mt-7 mb-4 h-[1px] w-full rounded-full"></div>
-        <div v-if="qrExist" class="text-oxford w-full text-center text-3xl font-bold tracking-wide leading-normal mb-2">
+        <div v-if="!!currentQR" class="bg-slate-500 mt-7 mb-4 h-[1px] w-full rounded-full"></div>
+        <div v-if="!!currentQR"
+          class="text-oxford w-full text-center text-3xl font-bold tracking-wide leading-normal mb-2">
           Ваш QR:</div>
         <div class="flex w-full justify-center">
           <div class="image-container max-w-xs max-h-xs">
 
           </div>
         </div>
+
       </div>
     </div>
   </div>
@@ -37,6 +41,31 @@ import { useToast } from '~/hooks/use-toast'
 import { qrService } from '~/services/qr.service'
 
 const { toast } = useToast()
+const file = ref<File[]>()
+const address = ref<string>('')
+const QRform = ref(null)
+const currentQR = ref<string>('')
+const isLoading = ref(false)
+const pdfRules = ref([
+  (value: File[] | null) => value ? (value.length > 0 || 'Обязательно загрузите файл') : false
+])
+const addressRules = ref([
+  (value: string) => !!value || 'Адрес не должен быть пустым',
+  (value: string) => {
+    const addressRegex = /^г\.\s[\p{L}\s]+,\s[\p{L}\s]+\s\d+(\/\d+)?,\s\d+\sэтаж$/u;
+    return addressRegex.test(value) || 'Формат адреса должен быть таким:\nг. {Город}, {Название адреса} {Номер дома}, {Номер этажа} этаж';
+  }
+])
+
+
+const downloadQR = () => {
+  const link = document.createElement('a')
+
+  link.setAttribute('href', currentQR.value)
+  link.download = 'iSec QR Code' + new Date(Date.now()).toDateString()
+  link.click()
+}
+
 const onSubmit = async (e: SubmitEventPromise) => {
   e.preventDefault();
   if (await isValid()) {
@@ -49,35 +78,17 @@ const onSubmit = async (e: SubmitEventPromise) => {
     const { url } = await qrService.createQR(form)
 
     const img = document.createElement('img')
-    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=320x320&data=${url}`
+    currentQR.value = `https://api.qrserver.com/v1/create-qr-code/?size=320x320&data=${url}`
+    img.src = currentQR.value
     img.className = 'w-full h-full object-cover mx-auto'
 
     document.querySelector('.image-container')?.appendChild(img)
-
     // on success    
     toast.success({ message: 'Загрузили ваш план эвакуации успешно. Вот ваш QR код. ' })
     isLoading.value = false
-    qrExist.value = true
     resetForm()
   }
-
 }
-
-const file = ref<File[]>()
-const address = ref<string>('')
-const QRform = ref(null)
-const qrExist = ref<boolean>(false)
-const pdfRules = ref([
-  (value: File[] | null) => value ? (value.length > 0 || 'Обязательно загрузите файл') : false
-])
-const addressRules = ref([
-  (value: string) => !!value || 'Адрес не должен быть пустым',
-  (value: string) => {
-    const addressRegex = /^г\.\s[\p{L}\s]+,\s[\p{L}\s]+\s\d+(\/\d+)?,\s\d+\sэтаж$/u;
-    return addressRegex.test(value) || 'Формат адреса должен быть таким:\nг. {Город}, {Название адреса} {Номер дома}, {Номер этажа} этаж';
-  }
-])
-const isLoading = ref(false)
 
 const isValid = async () => {
   //@ts-expect-error registerForm type gives automaticcaly in vuetify
@@ -89,8 +100,6 @@ const resetForm = () => {
   //@ts-expect-error registerForm type gives automaticcaly in vuetify
   QRform.value?.reset()
 }
-
-
 
 </script>
 
